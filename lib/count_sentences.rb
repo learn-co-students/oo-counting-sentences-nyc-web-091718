58ca4721667fe9486af102951417294973a21310
mx@E <mglxp5@gmail.com>
2018-09-18 19:45:46 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?('?')
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?('!')
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    a = self.split(/[.?!]/)
+    b = a.select {|str| str != ""}
+    b.size
   end
 end
+
+#binding.pry
