8974983e084e0644bb41e0e9519347f228040607
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-09-19 00:10:04 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+  # self.count(". ") + self.count("! ") + self.count("? ") + 1
+    sum = 0
+    self.split(/[.?!]/).each do |string| 
+      if string != "" 
+        sum += 1
+      end 
+       
+    end 
+    sum 
+ #   binding.pry
   end
 end
