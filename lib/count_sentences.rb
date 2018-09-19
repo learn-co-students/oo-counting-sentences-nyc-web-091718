1f7ebc6a8f282200d71ba3b329d6f5dc7a65043f
Frida Casas <fridakahsas@yahoo.com>
2018-09-19 01:55:54 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,24 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
-
+  
   def exclamation?
-
+    self.end_with?("!")
   end
-
+  
   def count_sentences
-
+    self.split(/[.!?]/).reject {|value| value.empty?}.size
   end
+  
+  def count_sentences_words
+    self.split(/[.!?]/).map{|value| !(value.match(/\w+/).nil?)}.reject{|value|  value== false}.size 
+ 
+  end
+
 end
