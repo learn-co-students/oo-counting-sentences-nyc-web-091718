e7db4c28a38196957e22c9e334554912e4f39cd6
asc5025 <albert.s.chun@gmail.com>
2018-09-18 20:16:29 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_sentence = self.split(/[?!.]+/)
+    #binding.pry
+    #\W+ Regexp pattern One or more non-word characters.
+    split_sentence.size
+    #binding.pry
   end
+
 end
